f38c33b1682c175400d1e59352517984550810bb
MMBazel <MMBazel@users.noreply.github.com>
2016-11-21 21:12:50 -0800

    Update test_script_final_project.sql

--- final_project/test_script_final_project.sql
+++ final_project/test_script_final_project.sql
@@ -2,50 +2,16 @@
 /*	University Database*/
 /* ****************************************************************************/
 -- Dropping Production Database and all tables and data
--- ????????????????
-
-DROP TABLE IF EXISTS DEPARTMENT;
-DROP TABLE IF EXISTS COURSE;
-DROP TABLE IF EXISTS FACULTY;
-DROP TABLE IF EXISTS STUDENT;
-DROP TABLE IF EXISTS TERM;
-DROP TABLE IF EXISTS SECTION;
-DROP TABLE IF EXISTS REGISTRATION;
-DROP TABLE IF EXISTS MAJOR;
-DROP TABLE IF EXISTS LOCATION;
-
-
-
-/* ****************************************************************************/
-
 DROP DATABASE IF EXISTS university;
-CREATE DATABASE university;
+CREATE DATABASE IF NOT EXISTS university;
 USE university;
---------------------------------------------------
-
-CREATE TABLE DEPARTMENT (
-    dept_id INT(2) UNSIGNED,
-    dept_name VARCHAR(30) NOT NULL,
-    budget INT(10) UNSIGNED, NOT NULL,
-    CONSTRAINT DEPT_PK PRIMARY KEY (dept_id)
-);
-
-
-CREATE TABLE COURSE (
-    course_id VARCHAR(8) ????? NOT NULL,
-    dept_id INT NOT NULL ????,
-    title VARCHAR(50) NOT NULL ???,
-    credits INT NULL,
-    prerequisite VARCHAR(8) NULL,
-    CONSTRAINT COURSE_PK PRIMARY KEY (course_id),
-    CONSTRAINT DEPT_FK FOREIGN KEY (dept_id)
-        REFERENCES DEPARTMENT (dept_id)
-);
-
+/* ****************************************************************************/
+/*	Create Tables*/
+-- 1	Faculty
 CREATE TABLE FACULTY (
     faculty_id INT(8) UNSIGNED,
     room_id INT(3) UNSIGNED NOT NULL,
-    dept_id INT ????? NOT NULL,
+    dept_id INT INT(2) UNSIGNED NOT NULL,
     name VARCHAR(15) NOT NULL,
     salary DECIMAL(10,2) UNSIGNED NOT NULL,
     CONSTRAINT FAC_PK PRIMARY KEY (faculty_id),
@@ -55,6 +21,78 @@ CREATE TABLE FACULTY (
         REFERENCES DEPARTMENT (dept_id)
 );
 
+-- 2	Location
+CREATE TABLE LOCATION (
+    room_id INT(3) UNSIGNED,
+    building VARCHAR(10) NOT NULL,
+    room_no INT(4) UNSIGNED NOT NULL,
+    capacity INT(3) UNSIGNED NOT NULL,
+    room_type ENUM('O','L','C') NOT NULL,
+    room_description ENUM('Office','Lab','Classroom') NOT NULL,
+    CONSTRAINT LOCAT_PK PRIMARY KEY (room_id),
+);
+
+-- 3	Term
+CREATE TABLE TERM (
+    term_id VARCHAR(5),
+    term_desc VARCHAR(12) UNIQUE NOT NULL ,
+    start_date DATE UNIQUE NOT NULL,
+    end_date DATE UNIQUE NOT NULL,
+    CONSTRAINT TERM_PK PRIMARY KEY (term_id)
+);
+
+-- 4	Department
+CREATE TABLE DEPARTMENT (
+    dept_id INT(2) UNSIGNED,
+    dept_name VARCHAR(30) NOT NULL,
+    budget INT(10) UNSIGNED, NOT NULL,
+    CONSTRAINT DEPT_PK PRIMARY KEY (dept_id)
+);
+
+-- 5	Course
+CREATE TABLE COURSE (
+    course_id VARCHAR(8),
+    dept_id INT(2) UNSIGNED NOT NULL,
+    title VARCHAR(50) NOT NULL,
+    credits INT(1) NOT NULL DEFAULT 0,
+    prerequisite VARCHAR(8),
+    CONSTRAINT COURSE_PK PRIMARY KEY (course_id),
+    CONSTRAINT DEPT_FK FOREIGN KEY (dept_id)
+        REFERENCES DEPARTMENT (dept_id)
+);
+
+-- 6	Section
+CREATE TABLE SECTION (
+    section_id INT(6) UNSIGNED,
+    course_id VARCHAR(8) NOT NULL,
+    term_id VARCHAR(5) NOT NULL,
+    faculty_id INT(8) UNSIGNED,
+    room_id INT(3) UNSIGNED NOT NULL,
+    section_number INT(3) UNSIGNED NOT NULL,
+    day SET('M','T','W','R','F') NOT NULL,
+    max_count INT(3) UNSIGNED NOT NULL,
+    start_time TIME DEFAULT 'TBA',
+    end_time TIME DEFAULT 'TBA',
+    CONSTRAINT SEC_PK PRIMARY KEY (section_id),
+    CONSTRAINT CRS_FK FOREIGN KEY (course_id)
+        REFERENCES COURSE (course_id),
+    CONSTRAINT TRM_FK FOREIGN KEY (term_id)
+        REFERENCES TERM (term_id),
+    CONSTRAINT FCTY_FK FOREIGN KEY (faculty_id)
+        REFERENCES FACULTY (faculty_id),
+    CONSTRAINT ROOM_FK FOREIGN KEY (room_id)
+        REFERENCES COURSE (course_id)
+);
+
+
+-- 7	Major
+CREATE TABLE MAJOR (
+    major_id INT(4) UNSIGNED,
+    major_desc VARCHAR(50) NOT NULL,
+    CONSTRAINT MAJOR_PK PRIMARY KEY (major_id)
+);
+
+-- 8	Student
 CREATE TABLE STUDENT (
     student_id INT(8) UNSIGNED,
     major_id INT(4) UNSIGNED NOT NULL,
@@ -72,70 +110,14 @@ CREATE TABLE STUDENT (
         REFERENCES MAJOR (major_id)
 );
 
-CREATE TABLE TERM (
-    term_id VARCHAR(5) ?????,
-    term_desc VARCHAR(12) UNIQUE NOT NULL ,
-    start_date DATE UNIQUE NOT NULL,
-    end_date DATE UNIQUE NOT NULL,
-    CONSTRAINT TERM_PK PRIMARY KEY (term_id)
-);
-
-
-CREATE TABLE SECTION (
-    section_id INT(6) UNSIGNED,
-    course_id VARCHAR(8) NOT NULL,
-    term_id VARCHAR(5) NOT NULL,
-    faculty_id INT INT(8) UNSIGNED,
-    room_id INT NOT NULL ?????,
-    section_number INT NOT NULL ?????,
-    day SET('M','T','W','R','F') ??????,
-    max_count INT NOT NULL ?????,
-    start_time TIME ???????,
-    end_time TIME ???????,
-    student_type ENUM('grad','ugrad') ????,
-    CONSTRAINT SEC_PK PRIMARY KEY (section_id),
-    CONSTRAINT CRS_FK FOREIGN KEY (course_id)
-        REFERENCES COURSE (course_id)
-        ON DELETE CASCADE ??????????,
-    CONSTRAINT TRM_FK FOREIGN KEY (term_id)
-        REFERENCES TERM (term_id)
-        ON DELETE CASCADE ??????????,
-    CONSTRAINT FCTY_FK FOREIGN KEY (faculty_id)
-        REFERENCES FACULTY (faculty_id)
-        ON DELETE CASCADE ??????????,
-    CONSTRAINT ROOM_FK FOREIGN KEY (room_id)
-        REFERENCES COURSE (course_id)
-        ON DELETE CASCADE ??????????
-);
-
+-- 9	Registration
 CREATE TABLE REGISTRATION (
-    student_id INT ?????,
-    section_id INT ???????,
-    midterm_grade ENUM('A','B','C','D','F','W') ?????,
-    final_grade ENUM('A','B','C','D','F','W') ?????,
+    student_id INT(8) UNSIGNED,
+    section_id INT INT(6) UNSIGNED,
+    midterm_grade ENUM('A','B','C','D','F','W'),
+    final_grade ENUM('A','B','C','D','F','W'),
     CONSTRAINT REGIS_PK PRIMARY KEY (student_id, section_id),
 );
-
-
-CREATE TABLE MAJOR (
-    major_id INT ?????,
-    major_desc VARCHAR(50) ???????,
-    CONSTRAINT MAJOR_PK PRIMARY KEY (major_id) ?????
-);
-
-
-
-CREATE TABLE LOCATION (
-    room_id INT ???????,
-    building VARCHAR(10) ?????,
-    room_no INT ??????,
-    capacity INT ????,
-    room_type ENUM('O','L','C'),
-    room_description ENUM('Office','Lab','Classroom'),
-    CONSTRAINT LOCAT_PK PRIMARY KEY (room_id),
-);
-
-
 
 /* ****************************************************************************/
 -- import data
